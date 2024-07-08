19ed3fcf899c4babfdc5e94a97cf0b20d347bd01
Giles Dring <giles@dringtech.com>
2024-07-08 11:11:44 +0100

    Add CMS

---
+++ _cms.ts
@@ -0,0 +1,32 @@
+import lumeCMS, { Fs } from "lume/cms/mod.ts";
+
+const cms = lumeCMS();
+
+// Configuration here
+cms.storage('tools', 'tools');
+
+cms.collection('tools', 'tools:*.md', [
+  "title: text",
+  "description: text",
+  "homepage: url",
+  {
+    name: "type",
+    type: "select",
+    options: [
+      "database",
+      "web-app",
+      "desktop-app",
+      "command-line-app",
+      "programming-language",
+      "library",
+      "search-engine",
+      "miscellaneous",
+      "site-generator",
+      "web-platform",
+      ""
+    ],
+  },
+  "content: markdown",
+])
+
+export default cms;
